98e7bd6be2951e7d7903f8f336f30b3776c55d74
wdthor <wdthor@proton.me>
2023-09-24 10:06:22 +0200

    Test github actions * Add unit-test.yml file
    * Edit unit-test.yml for using pnpm
    * Update Vite and Vitest packages versions
    * update vite config
    * add vitest globally
    * add fake test
    * remove vitest config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    globals: true
   }
 })
